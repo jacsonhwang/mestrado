5267ae29dc040cf673c97f7bf8394ee47aa16f16
arrudamichel <micheldarruda@yahoo.com.br>
2014-08-01 14:18:32 -0300

    Passagem do SVN da UFRJ para Git

---
+++ js/utilsCadastro.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+	// habilitar campo de formação acadêmica
+	$('#selectEscolaridade').on('change', function(){
+		if ($(this).val() <= parseInt("3"))
+			$("#inputFormacaoAcademica").attr("disabled", true);
+		else
+			$("#inputFormacaoAcademica").removeAttr("disabled");
+	});
+	
+	// impedir letras no campo de idade
+	$("#inputIdade").keydown(function(event){
+    
+        if (event.keyCode >= 37 && event.keyCode <= 40) { 
+              return event;
+        }
+        
+        if((event.keyCode != 8) && !((event.keyCode > 47 && event.keyCode < 58) || (event.keyCode > 95 && event.keyCode < 106))){ 
+            return false;
+        }
+    });
+});
