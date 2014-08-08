fb78814d0c3ff184c615c54a12b7b406b94410ee
arrudamichel <micheldarruda@yahoo.com.br>
2014-08-08 18:01:45 -0300

    email

--- email.php
+++ email.php
@@ -5,32 +5,48 @@ require("phpmailer/class.phpmailer.php");
 
 // Inicia a classe PHPMailer
 $mail = new PHPMailer();
+
+$mail->IsSMTP();
+$mail->SMTPAuth = true;
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = 465;
+$mail->SMTPSecure = 'ssl';
+$mail->Username = "michel.diasdearruda@gmail.com"; // my email which I hope to receive the data inputed on the field
+$mail->Password = "";
+
+$mail->SetFrom($email, 'Interessado'); // the email from the person who filled the form, that will be in the body of the message that I will receive
+$address = "micheldarruda@yahoo.com.br"; // my email which I hope to receive the data inputed on the field
+$mail->AddAddress($address, "Fine Design");
+$mail->Subject = "Fine Design - Avise me!";
+//$mail->MsgHTML($email);
+
+
 $mail->SMTPDebug = true;
 
 
 // Define os dados do servidor e tipo de conexão
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-$mail->IsSMTP(); // Define que a mensagem será SMTP
-$mail->Host = "smtp.dominio.net"; // Endereço do servidor SMTP
+/* $mail->IsSMTP(); // Define que a mensagem será SMTP
+$mail->Host = "smtp.gmail.com"; */ // Endereço do servidor SMTP
 //$mail->SMTPAuth = true; // Usa autenticação SMTP? (opcional)
 //$mail->Username = 'seumail@dominio.net'; // Usuário do servidor SMTP
 //$mail->Password = 'senha'; // Senha do servidor SMTP
 
 // Define o remetente
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-$mail->From = "michel.diasdearruda@gmail.com"; // Seu e-mail
-$mail->FromName = "yahoo"; // Seu nome
+/* $mail->From = "michel.diasdearruda@gmail.com"; // Seu e-mail
+$mail->FromName = "yahoo"; */ // Seu nome
 
 // Define os destinatário(s)
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-$mail->AddAddress('michel.diasdearruda@gmail.com', 'gmail');
-$mail->AddAddress('gomes.karine92@gmail.com');
+/* $mail->AddAddress('michel.diasdearruda@gmail.com', 'gmail');
+$mail->AddAddress('gomes.karine92@gmail.com'); */
 //$mail->AddCC('ciclano@site.net', 'Ciclano'); // Copia
 //$mail->AddBCC('fulano@dominio.com.br', 'Fulano da Silva'); // Cópia Oculta
 
 // Define os dados técnicos da Mensagem
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-$mail->IsHTML(true); // Define que o e-mail será enviado como HTML
+//								$mail->IsHTML(true); // Define que o e-mail será enviado como HTML
 //$mail->CharSet = 'iso-8859-1'; // Charset da mensagem (opcional)
 
 // Define a mensagem (Texto e Assunto)
