1aff54dbe212be5dcdf07030dfad9df7aa66bf9c
arrudamichel <micheldarruda@yahoo.com.br>
2014-08-11 15:17:30 -0300

    criação de email

---
+++ contato-erro.php
@@ -0,0 +1,15 @@
+<?php
+
+include 'login.php';
+
+
+
+?>
+
+<script>
+console.log("aaa");
+$(document).ready(function() {
+	
+	$("#alertaLogin").show();
+});
+</script>
---
+++ contato-sucesso.php
@@ -0,0 +1,19 @@
+<?php include 'header.php'; ?>
+
+<div class="container">
+	<div class="row">
+		<div class="col-lg-8 col-lg-offset-2">
+		
+			<div id="contatoTitulo" class="page-header">
+				<h1>Contato</h1>
+			</div>
+			
+			<div class="alert alert-success" role="alert">Contato realizado com sucesso.</div>
+			
+			<a href="index.php">Voltar para página inicial</a>
+			
+		</div>
+	</div>
+</div>
+
+<?php include 'footer.php'; ?>
