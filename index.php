fca13f3d2f626a303ced0d72abfbc5c9a0edafeb
jacsonhwang <jacsonhwang@gmail.com>
2014-09-13 10:32:51 -0300

    Atualização dos dados da página inicial.

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 			realizar tarefas que ajudem a reunir da forma mais precisa os diversos registros que representem a mesma pessoa.
 			Contribua com este projeto de forma divertida e inteligente. Desafie a si mesmo e tente executar as tarefas propostas.
 		</p>
-        <p><a href="saibaMais.php" class="btn btn-primary btn-lg" role="button">Saiba mais</a></p>
+        <p><a href="cadastro.php" class="btn btn-success btn-lg" role="button">Cadastre-se!!</a></p>
       </div>
     </div>
     
@@ -18,21 +18,18 @@
 	    <div class="row">
 	        <div class="col-md-4">
 	          <h2>Cadastre-se</h2>
-	          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, 
-	          ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-	          <p><a class="btn btn-default" href="cadastro.php" role="button">Saiba mais</a></p>
+	          <p>Todos são bem vindos a se cadastrarem na ferramenta GCER. Participe e ajude um amigo no seu experimento.</p>
+	          <p><a class="btn btn-default" href="cadastro.php" role="button">Cadastrar</a></p>
 	        </div>
 	        <div class="col-md-4">
 	          <h2>Jogue</h2>
-	          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, 
-	          ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-	          <p><a class="btn btn-default" href="jogo.php" role="button">Saiba mais</a></p>
+	          <p>Não sabe do que o GCER se trata? Confira em Saiba Mais uma breve explicação da ferramenta.</p>
+	          <p><a class="btn btn-default" href="saibaMais.php" role="button">Saiba mais</a></p>
 	       </div>
 	        <div class="col-md-4">
 	          <h2>Dúvidas</h2>
-	          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. 
-	          Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-	          <p><a class="btn btn-default" href="contato.php" role="button">Saiba mais</a></p>
+	          <p>Dúvidas, sugestões, críticas? Entre em contato e deixe uma mensagem.</p>
+	          <p><a class="btn btn-default" href="contato.php" role="button">Contato</a></p>
 	        </div>
 	      </div>
     </div>    
