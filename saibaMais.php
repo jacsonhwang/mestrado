61e04a8dcfff6dea4d16288d82c754334e0e912c
jacsonhwang <jacsonhwang@gmail.com>
2014-09-15 00:06:36 -0300

    Video Tutorial da ferramenta GCER.

--- saibaMais.php
+++ saibaMais.php
@@ -3,14 +3,21 @@
 <div id="formularioLogin" class="container">
 	<div class="row">
 		<div class="col-lg-8 col-lg-offset-2">
-			
+
 			<div id="sobreHomeTitulo" class="page-header">
-		<h1>GCER</h1>
+				<h1>GCER</h1>
 			</div>
+
+			<p>GCER é um projeto desenvolvido com fins acadêmicos e visa através
+				da sabedoria da multidão, realizar tarefas que ajudem a reunir da
+				forma mais precisa os diversos registros que representem o mesmo
+				objeto. Contribua com este projeto de forma divertida e inteligente.
+				Desafie a si mesmo e tente executar a tarefa proposta.</p>
+
 		
-	GCER é um projeto desenvolvido com fins acadêmicos e visa através da sabedoria da multidão,
-	realizar tarefas que ajudem a reunir da forma mais precisa os diversos registros que representem a mesma pessoa.
-	Contribua com este projeto de forma divertida e inteligente. Desafie a si mesmo e tente executar as tarefas propostas.
+			<video width="700" height="300" controls>
+			  <source src="video/GCER.mp4" type="video/mp4">
+			</video>
 		</div>
 	</div>
 </div>
